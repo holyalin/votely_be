f400a0f3b96a6752e23aa4dd93c417cec486e544
nazwayaqzhanoa <nazwayaqzhania@gmail.com>
2024-06-03 01:32:24 +0700

    checkpoint : be category

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+require('dotenv').config();
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const port = 3001;
 var cors = require("cors");
 const { register, login } = require("./controller/authController");
+const { showAllCategory } = require("./controller/categoryController");
 
 app.use(cors());
 // Middleware untuk parsing JSON
@@ -13,6 +16,8 @@ app.use(bodyParser.json());
 app.post("/register", register);
 app.post("/login", login);
 
+// Rute untuk kategori
+app.get("/category", showAllCategory)
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
