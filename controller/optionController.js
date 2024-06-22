e371b4292a6901da7effd8d6eda55c82cf46656f
arsyadal <arsyadal@student.telkomuniversity.ac.id>
2024-06-22 14:27:02 +0700

    checkpoint be

--- controller/optionController.js
+++ controller/optionController.js
@@ -6,17 +6,24 @@ const optionService = new OptionService();
 const createOption = async (req, res) => {
   try {
     const { name, image_url } = req.body;
-    const { id: owner_id } = req?.user;
-    let { polling_id } = req?.params;
+    const { id: owner_id } = req.user;
+    let { polling_id } = req.params;
     polling_id = parseInt(polling_id);
-    if (name.length < 1) throw new Error("Minimal judul option 1 karakter");
+
+    if (!name || name.length < 1) {
+      throw new Error("Nama opsi harus diisi dan minimal 1 karakter");
+    }
+
     const polling = await pollingService.pollingDetail(polling_id);
-    if (!polling) throw new Error("Polling tidak ditemukan");
+    if (!polling) {
+      throw new Error("Polling tidak ditemukan");
+    }
+
     const createOption = await optionService.createOption({ image_url, name, owner_id, polling_id });
     res.status(201).json({ success: true, data: createOption });
   } catch (error) {
-    console.error("error:", error);
-    res.status(400).json({ success: false, error: error?.message });
+    console.error("Error:", error.message);
+    res.status(400).json({ success: false, error: error.message });
   }
 };
 
